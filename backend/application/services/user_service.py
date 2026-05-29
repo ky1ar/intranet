0159b4354a89e1b976d4f0530c553ef4bbd38422
ky1ar <kenny@munoz.pe>
2026-05-29 15:59:17 +0000

    wip

--- backend/application/services/user_service.py
+++ backend/application/services/user_service.py
@@ -198,7 +198,7 @@ class UserService:
         default_page, _ = self.module_service.get_default_page(user.id)
 
         return {
-            "app_version": "1.6.8a",
+            "app_version": "1.6.8b",
             "id": user.id,
             "level_id": user.level_id,
             "department_id": user.department_id,
